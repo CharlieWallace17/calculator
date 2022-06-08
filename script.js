ff5bbbb232291dfe0c25a2cd1159f71e82293a40
GreenGimli <charlescwallace2017@gmail.com>
2022-06-08 14:17:59 -0500

    Add new button row. Create eventListener.
    eventListener updates display to add textContent from button clicked.

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const add = (x, y) => x + y;
-
 const subtract = (x, y) => x - y;
-
 const multiply = (x, y) => x * y;
-
 const divide = (x, y) => x / y;
 
 const operate = function(x, y, oper) {
@@ -12,3 +9,17 @@ const operate = function(x, y, oper) {
     if(oper === '*') return multiply(x, y); 
     if(oper === '/') return divide(x, y); 
 };
+
+const display = document.querySelector('.display');
+
+const buttons = document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', event => {
+        display.textContent += button.textContent;
+    });
+});
+
+
+
+
+
+
